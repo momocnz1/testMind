d129869c76468fea00ce1a7f9816d3cebc8681c4
momocn <119989172+momocnz1@users.noreply.github.com>
2023-12-18 10:58:48 +0700

    1

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module test {
+	requires junit;
+}
---
+++ src/test/run.java
@@ -0,0 +1,10 @@
+package test;
+
+public class run {
+
+	 public static void main(String[] args) {
+		 QuizOneJunit obj = new  QuizOneJunit();
+		 
+	}
+
+}
